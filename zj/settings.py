3eacf9a6ffa319a948998fb89f5e96ec2aac2a80
JamesOdera <nyangoma.odera@gmail.com>
2020-03-12 13:36:20 +0300

    add bootstrap 3 and 4 for my styling

--- zj/settings.py
+++ zj/settings.py
@@ -31,7 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'lee.apps.LeeConfig',
+    'lee',
+    'bootstrap3',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
