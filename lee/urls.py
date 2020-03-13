cc9503f86572fe965c6c47ac6f922bbf67ea4e73
JamesOdera <nyangoma.odera@gmail.com>
2020-03-13 10:46:21 +0300

    add a post where users can be able to create and post their projwects

--- lee/urls.py
+++ lee/urls.py
@@ -2,9 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
 from . import views
+from .views import PostListView
 
 urlpatterns=[
-    url(r'^$',views.index,name = 'index'),
+    url(r'^$',PostListView.as_view(),name = 'index'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- lee/views.py
+++ lee/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from django.views.generic import ListView
+import datetime as dt
+from .models import Post
 
 def index(request):
     return render(request, 'index.html')
+
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    ordering = ['-pub_date']
+    template_name = 'zj-lee/index.html'
