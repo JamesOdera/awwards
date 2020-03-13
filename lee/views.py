3cbbf47efb9778fa2c88eec05f09bb183f4d0341
JamesOdera <nyangoma.odera@gmail.com>
2020-03-13 11:25:48 +0300

    add a post block to the index page to view our posts

--- lee/views.py
+++ lee/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 import datetime as dt
 from .models import Post
 
 def index(request):
-    return render(request, 'index.html')
+    date = dt.date.today()
+    images= Image.objects.all()
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'zj-lee/index.html', {"date": date,"images": images},context)
 
 class PostListView(ListView):
     model = Post
     context_object_name = 'posts'
     ordering = ['-pub_date']
     template_name = 'zj-lee/index.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    
