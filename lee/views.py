3e32f3efb7fca4cf8346d1ca12037d37bf8ffc64
JamesOdera <nyangoma.odera@gmail.com>
2020-03-13 18:34:00 +0300

    return changes to its original state

--- lee/views.py
+++ lee/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 import datetime as dt
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin 
